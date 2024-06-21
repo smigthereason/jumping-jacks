eb722d54e74503c1fa080514a7271fbe13a2380a
stephy <stephshiro35@gmail.com>
2024-06-21 19:50:20 +0300

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Hero from "./pages/Hero";
 import "./App.css";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Hero />} />
       <Route path="/home" element={<Home />} />
-      {/* <Route path="/about-us" element={<AboutUs />} /> */}
-      {/* <Route path="/contacts" element={<Contacts />} /> */}
-      {/* <Route path="/services" element={<Services />} /> */}
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/portfolio" element={<Portfolio />} />
     </Routes>
   );
 };
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import "/src/styles/Contact.css";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaPowerOff,
+} from "react-icons/fa";
+
+import { MdEmail } from "react-icons/md";
+import { FaXTwitter } from "react-icons/fa6";
+// import Tag from "../components/Tag";
+
+const Contact: React.FC = () => {
+  return (
+    <div className="home-container">
+      {" "}
+      {/* Added a container class for styling */}
+      <div className="right-section">
+        {" "}
+        {/* Reversed: Left to Right */}
+        <div className="name">
+          <h2>Stephy</h2>
+        </div>
+        <div className="top-nav">
+          <div className="social-icons1">
+            <a
+              href="https://www.facebook.com/victor.maina.77312/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook size={24} />
+            </a>
+            <a
+              href="https://www.instagram.com/smigthereason/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram size={24} />
+            </a>
+
+            <a
+              href="https://x.com/SmigDs"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaXTwitter size={24} />
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/victor-maina-389318301/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin size={24} />
+            </a>
+
+            <a
+              href="mailto:victor.dmaina@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <MdEmail size={24} />
+            </a>
+          </div>
+          <button className="subscribe-btn">Subscribe</button>
+        </div>
+        <div className="content_block">
+        <div className="line"></div>
+          <span className="section_subtitle">contact</span>
+        </div>
+      </div>
+      <div className="left-section">
+        {/* Placeholder for content that was originally in the left section */}
+        <div className="tag">
+          <a href="/" rel="noopener noreferrer">
+            <FaPowerOff size={32} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
