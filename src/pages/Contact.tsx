5c44456eaf68fea1a95d7e439274e1ad639e1bfd
stephy <stephshiro35@gmail.com>
2024-06-25 12:35:05 +0300

    add cards

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,21 +6,27 @@ import {
   FaLinkedin,
   FaPowerOff,
 } from "react-icons/fa";
-
 import { MdEmail } from "react-icons/md";
 import { FaXTwitter } from "react-icons/fa6";
 import AOS from "aos";
 import "aos/dist/aos.css";
-// import Tag from "../components/Tag";
 
 const Contact: React.FC = () => {
+  React.useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div className="contact-container">
-      {" "}
-      {/* Added a container class for styling */}
       <div className="right-section">
-        {" "}
-        {/* Reversed: Left to Right */}
+      <div 
+                className="scrollable-container"
+                style={{
+                  height: 'auto',
+                  overflowY: 'auto',
+                  padding: '10px'
+                }}
+              >
         <div className="name1">
           <h2>Stephy</h2>
         </div>
@@ -40,7 +46,6 @@ const Contact: React.FC = () => {
             >
               <FaInstagram size={24} />
             </a>
-
             <a
               href="https://x.com/SmigDs"
               target="_blank"
@@ -48,7 +53,6 @@ const Contact: React.FC = () => {
             >
               <FaXTwitter size={24} />
             </a>
-
             <a
               href="https://www.linkedin.com/in/victor-maina-389318301/"
               target="_blank"
@@ -56,7 +60,6 @@ const Contact: React.FC = () => {
             >
               <FaLinkedin size={24} />
             </a>
-
             <a
               href="mailto:victor.dmaina@gmail.com"
               target="_blank"
@@ -80,17 +83,66 @@ const Contact: React.FC = () => {
           </div>
           <div className="section_block">
             <h2 className="section_title">
-              Let's get in touch and embark on new endavours!
+              Let's get in touch and embark on new endeavours!
             </h2>
-            <p className="contact_text">
-              I am geared and ready to to go! You can call or leave a request
-              here. I am always glad to have you here from 0900Hrs to 1800Hrs.{" "}
-            </p>
+            {/* <div className="card-container">
+              <div className="card">
+                <h3>Card 1</h3>
+                <p>Content for card 1</p>
+              </div>
+              <div className="card">
+                <h3>Card 2</h3>
+                <p>Content for card 2</p>
+              </div>
+              <div className="card">
+                <h3>Card 3</h3>
+                <p>Content for card 3</p>
+              </div>
+            </div> */}
+     <div className="container">
+    
+  <div className="grid">
+    <div className="card">
+      <div className="card-image">
+        <img src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="Woman in white dress" />
+      </div>
+      <div className="card-overlay"></div>
+      <div className="card-content">
+        <h1>Beauty</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
+        <button>See More</button>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-image">
+        <img src="https://images.unsplash.com/photo-1494145904049-0dca59b4bbad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" alt="Person wearing denim jacket" />
+      </div>
+      <div className="card-overlay"></div>
+      <div className="card-content">
+        <h1>Beyond Builder</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
+        <button>See More</button>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-image">
+        <img src="https://images.unsplash.com/photo-1502675135487-e971002a6adb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" alt="Person in white dress on beach" />
+      </div>
+      <div className="card-overlay"></div>
+      <div className="card-content">
+        <h1>Shooting Star</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
+        <button>See More</button>
+      </div>
+    </div>
+  </div>
+ 
+</div>
           </div>
         </div>
       </div>
+      </div>
       <div className="left-section">
-        {/* Placeholder for content that was originally in the left section */}
         <div className="tag">
           <a href="/" rel="noopener noreferrer">
             <FaPowerOff size={32} />
